ee76f9c4f46dea436f644fe3ccf08a3b3c7d5f6d
I Komang Adi Wirata <mangadiwirata578@gmail.com>
2026-08-06 13:50:13 +0800

    Tambah data mangadi ke Wall of Interns

--- src/interns.js
+++ src/interns.js
@@ -11,6 +11,7 @@
 
 const interns = [
   { name: "Daniel", role: "DevOps", funFact: "Memandu sesi onboarding ini" },
-];
+  { name: "Mangadi", role: "FullStack Dev", funFact: "Baru belajar DevOps" },
+];  
 
 module.exports = interns;
