12b38e2b58e53edd87345f7eb066bc12c80913ac
pablooqt <106124645+pablooqt@users.noreply.github.com>
2026-08-06 14:37:20 +0800

    add data

--- src/interns.js
+++ src/interns.js
@@ -11,7 +11,7 @@
 
 const interns = [
   { name: "Daniel", role: "DevOps", funFact: "Memandu sesi onboarding ini" },
-  { name: "Satria",
+  { name: "Satriaa",
     role: "Fullstack Developer",
     funFact: "Suka kopi tanpa gula" 
   },
